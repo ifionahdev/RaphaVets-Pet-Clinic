6167c74bdd90dbcb4744ec3c8df2745502cd37e7
Lumino <mmapili.k12150551@umak.edu.ph>
2025-10-25 19:30:33 +0800

    Add files via upload
    database bes

---
+++ raphavets_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2025 at 01:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `raphavets_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_tbl`
+--
+
+CREATE TABLE `account_tbl` (
+  `accId` int(11) NOT NULL,
+  `firstName` varchar(250) NOT NULL,
+  `lastName` varchar(250) NOT NULL,
+  `eMail` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `datetimeCreated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account_tbl`
+--
+
+INSERT INTO `account_tbl` (`accId`, `firstName`, `lastName`, `eMail`, `password`, `datetimeCreated`) VALUES
+(2, 'Mark', 'Mapili', 'markmapili29@gmail.com', '$2b$10$BQZcuX9rPNcSwTmwmaIaE.lw/uT7.lQXIevkgG9f1pBY8fIpEZX0K', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account_tbl`
+--
+ALTER TABLE `account_tbl`
+  ADD PRIMARY KEY (`accId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account_tbl`
+--
+ALTER TABLE `account_tbl`
+  MODIFY `accId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
