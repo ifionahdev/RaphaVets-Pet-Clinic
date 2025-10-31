55c03b0d6b6076235ce4e4e484b3de340025ed50
Lumino <mmapili.k12150551@umak.edu.ph>
2025-10-31 18:11:42 +0800

    Modify account_tbl structure and data
    Updated email field and added userType to account_tbl.

--- raphavets_db.sql
+++ raphavets_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2025 at 09:55 AM
+-- Generation Time: Oct 31, 2025 at 11:09 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,18 +31,20 @@ CREATE TABLE `account_tbl` (
   `accId` int(11) NOT NULL,
   `firstName` varchar(250) NOT NULL,
   `lastName` varchar(250) NOT NULL,
-  `eMail` varchar(250) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `password` varchar(250) NOT NULL,
-  `datetimeCreated` datetime NOT NULL
+  `datetimeCreated` datetime NOT NULL,
+  `userType` varchar(11) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account_tbl`
 --
 
-INSERT INTO `account_tbl` (`accId`, `firstName`, `lastName`, `eMail`, `password`, `datetimeCreated`) VALUES
-(2, 'Mark', 'Mapili', 'markmapili29@gmail.com', '$2b$10$BQZcuX9rPNcSwTmwmaIaE.lw/uT7.lQXIevkgG9f1pBY8fIpEZX0K', '0000-00-00 00:00:00'),
-(3, 'Fionah Irish', 'Beltran', 'soupcuppy@gmail.com', '$2b$10$l/lPrlJ8Vho/LyqoOiq2sOlSSrZ1t.atCEgMaxBBOW05jri/FfwIS', '0000-00-00 00:00:00');
+INSERT INTO `account_tbl` (`accId`, `firstName`, `lastName`, `email`, `password`, `datetimeCreated`, `userType`) VALUES
+(2, 'Mark', 'Mapili', 'markmapili29@gmail.com', '$2b$10$BQZcuX9rPNcSwTmwmaIaE.lw/uT7.lQXIevkgG9f1pBY8fIpEZX0K', '0000-00-00 00:00:00', 'admin'),
+(3, 'Fionah Irish', 'Beltran', 'soupcuppy@gmail.com', '$2b$10$l/lPrlJ8Vho/LyqoOiq2sOlSSrZ1t.atCEgMaxBBOW05jri/FfwIS', '0000-00-00 00:00:00', 'admin'),
+(5, 'mark', 'mapili', 'markmapili72@gmail.com', '$2b$10$LMTrRhOAEKAweVGBy1NXQeGCWEzgN2d5WueonGDiRibvDGER08YVe', '0000-00-00 00:00:00', 'user');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +64,7 @@ ALTER TABLE `account_tbl`
 -- AUTO_INCREMENT for table `account_tbl`
 --
 ALTER TABLE `account_tbl`
-  MODIFY `accId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `accId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
