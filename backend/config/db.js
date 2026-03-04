51523259f06eba8f392523586d440c2e5dbe1a63
pinakamasarap <171237970+ifionahdev@users.noreply.github.com>
2026-03-04 15:00:59 +0800

    changed connectivity for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // Create a promise-based pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -17,7 +15,7 @@ const db = mysql.createPool({
 // Test connection
 try {
   const connection = await db.getConnection();
-  console.log("✅ Connected to MySQL database!");
+  console.log("✅ Connected to Railway MySQL database!");
   connection.release();
 } catch (err) {
   console.error("❌ Database connection failed:", err.message);
